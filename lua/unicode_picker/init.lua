a40de83724df486ef94a2bd1a2312090c8dfd303
Thom Dickson <td3of4@gmail.com>
2023-12-17 21:22:19 -0500

    Pull unicode data from unicode consortium

--- lua/unicode_picker/init.lua
+++ lua/unicode_picker/init.lua
@@ -11,22 +11,23 @@ M.unicode_chars = function(opts)
     opts = opts or themes.get_cursor()
     return pickers.new(opts, {
         prompt_title = "Pick a thing",
-        finder = finders.new_table {
+        finder = finders.new_table({
             results = require("unicode_picker.chars"),
             entry_maker = function(entry)
                 return {
                     value = entry,
-                    display = entry[1] .. ": " .. entry[2],
-                    ordinal = entry[1],
+                    display = "[" .. entry[1] .. "]: " ..
+                        table.concat(entry, " ", 2),
+                    ordinal = table.concat(entry, " ", 2),
                 }
             end
-        },
+        }),
         sorter = conf.generic_sorter(opts),
         attach_mappings = function(prompt_bufnr, map)
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
                 local selection = action_state.get_selected_entry().value
-                vim.api.nvim_put({ selection[2] }, "", false, true)
+                vim.api.nvim_put({ selection[1] }, "", false, true)
                 vim.api.nvim_input("a") -- hack to get back into insert mode
             end)
             return true
